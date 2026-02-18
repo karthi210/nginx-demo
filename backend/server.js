684c811e7cbca099b270869d2df4b318296873ce
karthi210 <119928109+karthi210@users.noreply.github.com>
2026-02-18 17:50:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/message", (req, res) => {
-  res.json({ message: "Hello from Backend via Jenkins + Nginx 🚀" });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String
 });
 
-app.listen(8000, () => {
-  console.log("Backend running on port 8000");
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running 🚀");
 });
+
+app.post("/api/users", async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.listen(8000, () => console.log("Server running on 8000"));
